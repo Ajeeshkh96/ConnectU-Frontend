5d20daf0cdda0ba719a1176310ce16a4a22e54aa
Ajeesh K H <ajeeshkh786@gmail.com>
2024-02-22 18:52:19 +0530

    base url changed

--- src/config.js
+++ src/config.js
@@ -4,9 +4,9 @@ const currentProtocol = window.location.protocol;
 
 // const BASE_URL = 'https://www.arunkrishna.online';
 
-// const BASE_URL = 'http://13.49.44.20';
+const BASE_URL = 'http://54.253.129.14';
 
-const BASE_URL = 'http://localhost:8000';
+// const BASE_URL = 'http://localhost:8000';
 
 
 
