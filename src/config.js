87f4d813c1bba835329fd0f9a758329dde4c1170
Ajeesh K H <ajeeshkh786@gmail.com>
2024-03-15 14:11:22 +0530

    base_url changed

--- src/config.js
+++ src/config.js
@@ -4,10 +4,10 @@ const currentProtocol = window.location.protocol;
 
 // const BASE_URL = 'https://www.ajeesh.online';
 
-// const BASE_URL = 'http://0.0.0.0:9090';
+const BASE_URL = 'http://0.0.0.0:9090';
 
 // const BASE_URL = 'http://localhost:8000';
 
-const BASE_URL = 'http://3.110.37.167';
+// const BASE_URL = 'http://3.110.37.167';
 
 export { BASE_URL };
